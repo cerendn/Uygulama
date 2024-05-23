0e664a265dcd2d7ab9ddbcea2b57ccee2881ca1e
cerendn <cerendncer@outlook.com>
2024-05-23 14:40:13 +0300

    useState

--- my-project/src/components/Home.jsx
+++ my-project/src/components/Home.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Home = () => {
   return (
     <div>
-      <h2>Welcome to do Recipe Sharing Platform</h2>
+      <h2>Welcome to do Natural Mask Recipe Sharing Platform</h2>
       <p>Find and Share the best mask recipe</p>
     </div>
   );
--- my-project/src/components/NewResipeForm.jsx
+++ my-project/src/components/NewResipeForm.jsx
@@ -2,10 +2,14 @@ import React, { useState } from "react";
 
 const NewResipeForm = () => {
   const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
 
   const handleSubmit = (event) => {
     event.preventDefault();
     setTitle("");
+    setDescription("");
+    setImageUrl("");
   };
 
   return (
@@ -18,8 +22,17 @@ const NewResipeForm = () => {
           required
           onChange={(event) => setTitle(event.target.value)}
         />
-        <textarea placeholder="Mask Resipe Description" required />
-        <input type="text" placeholder="image Url" required />
+        <textarea
+          placeholder="Mask Resipe Description"
+          required
+          onChange={(event) => setDescription(event.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="image Url"
+          required
+          onChange={(event) => setImageUrl(event.target.value)}
+        />
         <button type="submit">Add Mask</button>
       </form>
     </div>
