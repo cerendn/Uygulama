49e27655d2733f0d8ba37c368108ad8a4686dca0
cerendn <cerendncer@outlook.com>
2024-05-24 12:22:05 +0300

    db.json

--- my-project/src/components/NewResipeForm.jsx
+++ my-project/src/components/NewResipeForm.jsx
@@ -5,6 +5,7 @@ const NewResipeForm = () => {
   const [description, setDescription] = useState("");
   const [imageUrl, setImageUrl] = useState("");
 
+  //sayfanın yeniden refresh edilmesi engeller ve inputları temizler
   const handleSubmit = (event) => {
     event.preventDefault();
     setTitle("");
@@ -23,11 +24,13 @@ const NewResipeForm = () => {
           onChange={(event) => setTitle(event.target.value)}
         />
         <textarea
+          value={description}
           placeholder="Mask Resipe Description"
           required
           onChange={(event) => setDescription(event.target.value)}
         />
         <input
+          value={imageUrl}
           type="text"
           placeholder="image Url"
           required
