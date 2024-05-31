aa3b963ec19f15678aeada949648c1012dd2b5f0
cerendn <cerendncer@outlook.com>
2024-05-31 12:10:51 +0300

    css cont

--- my-project/src/components/RecipeCard.jsx
+++ my-project/src/components/RecipeCard.jsx
@@ -4,10 +4,11 @@ const RecipeCard = ({ title, description, imageUrl, deleteRecipe, id }) => {
   const [isDeletedLoading, setIsDeletedLoading] = useState(false);
   return (
     <div className="recipe-card">
-      <img src={imageUrl} alt={title} />
+      <img className="recipe-image" src={imageUrl} alt={title} />
       <h4> {title} </h4>
       <p> {description} </p>
       <button
+        className="btn-delete"
         //birlikte calıstıgı islemlerden ayrı calısması icin async özl ekleyip tüm btnların loading olmasını engellenir,settimeout ile de yapılabilir
         onClick={async () => {
           setIsDeletedLoading(true);
@@ -15,7 +16,7 @@ const RecipeCard = ({ title, description, imageUrl, deleteRecipe, id }) => {
           setIsDeletedLoading(false);
         }}
       >
-        {isDeletedLoading ? "Deleting..." : "Deleted"}
+        {isDeletedLoading ? "Deleting..." : "Delete"}
       </button>
     </div>
   );
