53a2a2352b5cd79699cbfd42813a79f52a7afe28
cerendn <cerendncer@outlook.com>
2024-06-08 11:48:10 +0300

    AuthServise

--- my-project/src/services/AuthServise.jsx
+++ my-project/src/services/AuthServise.jsx
@@ -1,25 +1,50 @@
 //mock authentication service
-
+import axios from "axios";
 import Login from "../components/Login";
 //ilgili yerde calısacak(serviste calısacak)
+// const AuthService = {
+//   isAuthenticated: false,
+//   login(username, password) {
+//     //In a real app, you'd have API calls here.
+//     //This is just a mockup,so we'll simulate async with a promise
+//     return new Promise((resolve, reject) => {
+//       setTimeout(() => {
+//         if (username === "admin" && password === "password") {
+//           this.isAuthenticated = true;
+//           resolve();
+//         } else {
+//           reject();
+//         }
+//       }, 2000);
+//     });
+//   },
+//   logout() {
+//     this.isAuthenticated = false;
+//   },
+// };
 const AuthService = {
-  isAuthenticated: false,
-  login(username, password) {
-    //In a real app, you'd have API calls here.
-    //This is just a mockup,so we'll simulate async with a promise
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        if (username === "admin" && password === "password") {
-          this.isAuthenticated = true;
-          resolve();
-        } else {
-          reject();
-        }
-      }, 2000);
-    });
+  login: async (username, password) => {
+    const response = await axios.post(
+      "https://api.escuelajs.co/api/v1/auth/login",
+      {
+        email: username,
+        password: password,
+      }
+    );
+    console.log(response);
+    if (response.data.access_token) {
+      //localStorage'a setItem ile bilgiler json a cevrilip kaydedilir
+      localStorage.setItem("user", JSON.stringify(response.data));
+    }
+    return response.data;
   },
-  logout() {
-    this.isAuthenticated = false;
+
+  logout: () => {
+    localStorage.removeItem("user");
+  },
+  getCurrentUser: () => {
+    //alınırkende jsona cevrilip alınıyor
+    return JSON.parse(localStorage.getItem("user"));
   },
 };
 
