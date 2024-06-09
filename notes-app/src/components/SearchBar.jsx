e07b9f71262f30e941ded3a6d71c1708bddf8dcb
cerendn <cerendncer@outlook.com>
2024-06-09 11:07:35 +0300

    search

---
+++ notes-app/src/components/SearchBar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const SearchBar = () => {
+  return (
+    <div className="searchBar">
+      <input type="text" placeholder="Not Ara..." />
+      <button>Ara</button>
+    </div>
+  );
+};
+
+export default SearchBar;
