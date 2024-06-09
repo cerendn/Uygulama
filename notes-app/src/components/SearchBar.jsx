439cea6fb944fc0f8cd381510ffce244788f9247
cerendn <cerendncer@outlook.com>
2024-06-09 11:31:17 +0300

    axios

--- notes-app/src/components/SearchBar.jsx
+++ notes-app/src/components/SearchBar.jsx
@@ -1,10 +1,33 @@
 import React from "react";
+import { useState } from "react";
+import axios from "axios";
 
 const SearchBar = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [resultProducts, setResultProducts] = useState([]);
+
+  const handleInputValueChange = (event) => {
+    setInputValue(event.target.value);
+  };
+  const handleSearchButton = async () => {
+    try {
+      const response = await axios.get(
+        "https://api.escuelajs.co/api/v1/products/?title={inputValue}"
+      );
+      console.log(response.data);
+    } catch (error) {
+      console.error("Veri çekme hatası:", error);
+    }
+  };
   return (
     <div className="searchBar">
-      <input type="text" placeholder="Not Ara..." />
-      <button>Ara</button>
+      <input
+        type="text"
+        placeholder="Not Ara..."
+        value={inputValue}
+        onChange={handleInputValueChange}
+      />
+      <button onClick={handleSearchButton}>Ara</button>
     </div>
   );
 };
